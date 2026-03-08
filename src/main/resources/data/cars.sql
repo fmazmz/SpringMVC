dfa22ecf27c21066781e9621644ec80703b84bef
WHITEROSE <firasmoussa60@gmail.com>
2026-03-08 02:08:09 +0100

    extend cars.sql with more entrie

--- src/main/resources/data/cars.sql
+++ src/main/resources/data/cars.sql
@@ -1,2 +1,34 @@
-INSERT INTO car (id, make, model, hourly_price, licence_plate, vin, year)
-VALUES (gen_random_uuid(), 'Toyota', 'Corolla', 1000, 'ABC123', '5TDBT48A45S235008', 2020);
+INSERT INTO car (id, make, model, hourly_price, licence_plate, vin, year) VALUES
+                                                                              (gen_random_uuid(), 'Volvo', 'XC60', 220, 'ABD123', 'YV1UZK5V6N1234501', 2022),
+                                                                              (gen_random_uuid(), 'Volvo', 'V60', 200, 'DEF456', 'YV1ZWK5V1N1234502', 2021),
+                                                                              (gen_random_uuid(), 'Volvo', 'XC90', 300, 'GHI789', 'YV4A22PK8M1234503', 2023),
+                                                                              (gen_random_uuid(), 'Volvo', 'S60', 190, 'JKL012', 'YV1RSK2V5L1234504', 2020),
+                                                                              (gen_random_uuid(), 'Volvo', 'V90', 230, 'MNO345', 'YV1PWK5V0N1234505', 2022),
+                                                                              (gen_random_uuid(), 'Volvo', 'XC40', 210, 'PQR678', 'YV1XZK5V7M1234506', 2021),
+                                                                              (gen_random_uuid(), 'Volvo', 'V70', 140, 'STU901', 'YV1BW7240A1234507', 2016),
+                                                                              (gen_random_uuid(), 'Volvo', 'S90', 240, 'VWX234', 'YV1PSK5V3N1234508', 2023),
+                                                                              (gen_random_uuid(), 'Volvo', 'C30', 120, 'YZA567', 'YV1MK6822C1234509', 2015),
+                                                                              (gen_random_uuid(), 'Volvo', 'XC70', 150, 'BCD890', 'YV1BZ8256F1234510', 2017),
+
+                                                                              (gen_random_uuid(), 'Polestar', '2', 280, 'EFG234', 'LPSVSEDE1NF1234511', 2023),
+                                                                              (gen_random_uuid(), 'Polestar', '2 Long Range', 300, 'HIJ567', 'LPSVSEDE2NF1234512', 2024),
+
+                                                                              (gen_random_uuid(), 'Saab', '9-3', 110, 'KLM890', 'YS3FB49Y881234513', 2010),
+                                                                              (gen_random_uuid(), 'Saab', '9-5', 120, 'NOP123', 'YS3ED49G7A1234514', 2011),
+
+                                                                              (gen_random_uuid(), 'Volkswagen', 'Golf', 140, 'QRS456', 'WVWZZZ1KZAW1234515', 2020),
+                                                                              (gen_random_uuid(), 'Volkswagen', 'Passat', 160, 'TUV789', 'WVWZZZ3CZJE1234516', 2021),
+
+                                                                              (gen_random_uuid(), 'Skoda', 'Octavia', 130, 'WXY321', 'TMBJG7NE5H01234517', 2020),
+                                                                              (gen_random_uuid(), 'Skoda', 'Superb', 170, 'ZAB654', 'TMBEN9NP4K71234518', 2022),
+
+                                                                              (gen_random_uuid(), 'Toyota', 'RAV4', 180, 'CDE987', 'JTMBFREV7HJ1234519', 2021),
+                                                                              (gen_random_uuid(), 'Toyota', 'Corolla', 120, 'FGH210', 'JTDBR32E7201234520', 2019),
+
+                                                                              (gen_random_uuid(), 'Kia', 'Ceed', 110, 'IJK543', 'U5YHN816AFL1234521', 2019),
+                                                                              (gen_random_uuid(), 'Hyundai', 'i30', 115, 'LMN876', 'KMHDH41EMAU1234522', 2020),
+
+                                                                              (gen_random_uuid(), 'BMW', '320d', 210, 'OPQ109', 'WBA8E9G50GNU1234523', 2022),
+                                                                              (gen_random_uuid(), 'Audi', 'A4', 220, 'RST432', 'WAUZZZ8K1AA1234524', 2021),
+
+                                                                              (gen_random_uuid(), 'Mercedes-Benz', 'C220', 230, 'UVW765', 'WDDGF8AB9EA1234525', 2022);
