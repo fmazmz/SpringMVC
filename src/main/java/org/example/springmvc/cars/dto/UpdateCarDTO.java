1e3b329fe85fb595957982898ad32eabdc5616bb
Kristina M <kristina0x7@gmail.com>
2026-03-10 09:46:00 +0100

    feat: updateCarDTO

---
+++ src/main/java/org/example/springmvc/cars/dto/UpdateCarDTO.java
@@ -0,0 +1,30 @@
+package org.example.springmvc.cars.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.Year;
+
+public record UpdateCarDTO(
+
+        @NotBlank
+        String make,
+
+        @NotBlank
+        String model,
+
+        @NotNull
+        @DecimalMin(value = "0", inclusive = true)
+        BigDecimal hourlyPrice,
+
+        @NotBlank
+        @Size(min = 6, max = 6)
+        String licencePlate,
+
+        @NotBlank
+        String vin,
+
+        @NotNull
+        @PastOrPresent
+        Year year
+) {}
